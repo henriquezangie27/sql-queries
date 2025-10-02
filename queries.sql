7b53fbd57ac46ca653275d4d93ace98fc0af29a4
Angie Henriquez <henriquezangie27@gmail.com>
2025-10-02 14:15:19 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,3 +27,20 @@ SELECT DATE_TRUNC('month', sale_date) AS month,
 FROM sales
 GROUP BY month
 ORDER BY month;
+
+-- 5. Actualizar el correo electrónico de un cliente
+UPDATE customers
+SET email = 'nuevoemail@example.com'
+WHERE customer_id = 101;
+
+-- 6. Eliminar una venta específica
+DELETE FROM sales
+WHERE sale_id = 5001;
+
+-- 7. Obtener nombre del cliente y productos comprados
+SELECT c.first_name, c.last_name, p.product_name, s.quantity
+FROM sales s
+JOIN customers c ON s.customer_id = c.customer_id
+JOIN products p ON s.product_id = p.product_id
+ORDER BY c.last_name
+
